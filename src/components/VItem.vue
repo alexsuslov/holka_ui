a0004428686bae0ce503191d164c77e1c4df139a
isuslov <isuslov@me.com>
2024-01-14 15:47:31 +0300

    1

--- src/components/VItem.vue
+++ src/components/VItem.vue
@@ -5,13 +5,13 @@ import { FwbCard, FwbImg } from 'flowbite-vue'
 <template>
   <FwbCard href="#">
     <div class="w-full flex flex-col p-5">
-      <FwbImg alt="item_image" :src="img" class="w-[250px] h-[250px]" />
+      <FwbImg alt="item_image" src="" class="w-[250px] h-[250px]" />
       <div>
-        <p class="mb-2 text-2xl font-bold text-white">{{ title || 'название' }}</p>
-        <h2 class="mb-2 text-white font-bold text-2xl">{{ price || 'цена' }}</h2>
-        <p class="text-white">{{ location || 'Санкт-Петербург, Россия' }}</p>
+        <p class="mb-2 text-2xl font-bold text-white">{{ 'название' }}</p>
+        <h2 class="mb-2 text-white font-bold text-2xl">{{ 'цена' }}</h2>
+        <p class="text-white">{{ 'Санкт-Петербург, Россия' }}</p>
         <p class="font-normal text-gray-400">
-          {{ createdOn || `Created On ${new Date().toLocaleDateString()}` }}
+          {{ `Created On ${new Date().toLocaleDateString()}` }}
         </p>
       </div>
     </div>
